243e6af4076be0f1e8bca1af10a69e3dcaded62e
hari5722 <rharikrishnanaik160@gmail.com>
2025-05-15 00:06:14 +0530

    changes

--- JS/cartcount.js
+++ JS/cartcount.js
@@ -1,13 +1,13 @@
-// homecart.js
+// // homecart.js
 
-function updateCartCount() {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const count = cart.reduce((acc, item) => acc + item.quantity, 0);
-    const countElement = document.getElementById('cart-count');
-    if (countElement) {
-        countElement.textContent = count;
-    }
-}
+// function updateCartCount() {
+//     const cart = JSON.parse(localStorage.getItem('cart')) || [];
+//     const count = cart.reduce((acc, item) => acc + item.quantity, 0);
+//     const countElement = document.getElementById('cart-count');
+//     if (countElement) {
+//         countElement.textContent = count;
+//     }
+// }
 
-// Run on every page load
-document.addEventListener('DOMContentLoaded', updateCartCount);
+// // Run on every page load
+// document.addEventListener('DOMContentLoaded', updateCartCount);
